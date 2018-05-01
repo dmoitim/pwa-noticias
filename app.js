66ce73651989d25a5551da0d4dc605af93a3b2f0
Devair Luiz Moitim <dmoitim@gmail.com>
2018-05-01 20:23:07 -0300

    Inclusão do combo para seleção da fonte.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const apiKey = '893e2c2053864988bbaedb9f315bbc8a';
 const newsArticles = document.querySelector('main');
+const sourceSelector = document.querySelector('#sources');
 
 window.addEventListener('load', e => {
     updateNews();
+    updateNewsSources();
 });
+
+async function updateNewsSources() {
+    const resp = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
+    const json = await resp.json();
+    sourceSelector.innerHTML =
+        json.sources
+            .map(source => `<option value="${source.id}">${source.name}</option>`)
+            .join('\n');
+}
 
 async function updateNews() {
     newsArticles.innerHTML = '';
@@ -16,13 +27,11 @@ async function updateNews() {
 function createArticle(article) {
     return `
         <div class="article">
-            <a href="${article.url}">
-                <h2>${article.title}</h2>
-                <img src="${article.urlToImage}">
-                <p>${article.description}</p>
-            </a>
+        <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}" alt="${article.title}">
+            <p>${article.description}</p>
+        </a>
         </div>
     `;
 }
-
-
